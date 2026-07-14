8cb3d1165abd8d8bcdd549a91fcd657c51d39d8c
Samuele Basso <samuele.basso@mail.polimi.it>
2026-07-14 12:45:20 +0200

    Buffer
    Implemented buffer with tests. Note: the buffer sits in between the
    thread that reads HTML and related resources from disk and/or the
    network and the thread that consumes such resources.

---
+++ source/Buffer.cpp
@@ -0,0 +1,50 @@
+#include "Buffer.h"
+
+#include <cstring>
+
+namespace Crawler
+{
+
+    void Buffer::PeekInternal(std::byte* destination, size_t* readBytes, size_t count, std::unique_lock<std::mutex>& lock) {
+        this->p_CanRead.wait(lock, [this] {
+            return !this->p_InternalBuffer.empty();
+        });
+
+        size_t bytesToRead = std::min(count, this->p_InternalBuffer.size());
+        *readBytes = bytesToRead;
+
+        std::memcpy(destination, this->p_InternalBuffer.data(), bytesToRead * sizeof(std::byte));
+    }
+
+    void Buffer::Write(const std::byte* source, size_t count) {
+        std::unique_lock<std::mutex> lock(this->p_Mutex);
+
+        size_t necessarySize = this->p_InternalBuffer.size() + count;
+        if (necessarySize > this->p_InternalBuffer.capacity()) {
+            this->p_InternalBuffer.reserve(necessarySize);
+        }
+
+        this->p_InternalBuffer.insert(this->p_InternalBuffer.end(), source, source + count);
+        this->p_CanRead.notify_one();
+    }
+
+    void Buffer::Read(std::byte* destination, size_t* readBytes, size_t count) {
+        std::unique_lock<std::mutex> lock(this->p_Mutex);
+
+        this->PeekInternal(destination, readBytes, count, lock);
+        this->p_InternalBuffer.erase(this->p_InternalBuffer.begin(), this->p_InternalBuffer.begin() + *readBytes);
+    }
+
+    void Buffer::Peek(std::byte* destination, size_t* readBytes, size_t count) {
+        std::unique_lock<std::mutex> lock(this->p_Mutex);
+
+        this->PeekInternal(destination, readBytes, count, lock);
+    }
+
+    size_t Buffer::GetSize() {
+        std::unique_lock<std::mutex> lock(this->p_Mutex);
+
+        return this->p_InternalBuffer.size();
+    }
+
+}
